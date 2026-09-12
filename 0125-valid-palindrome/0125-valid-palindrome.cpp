f78d0074f875b544120290ffb2e0972f28a3d974
Sachi Somankar <somankarsachi@gmail.com>
2026-09-12 17:56:08 +0530

    Time: 0 ms (100%), Space: 9.8 MB (76.98%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    bool check(string &s, int left, int right)
+    {
+        while(left < right && !isalnum(s[left]))
+            left++;
+
+        while(left < right && !isalnum(s[right]))
+            right--;
+
+        if(left >= right)
+            return true;
+
+        if(tolower(s[left]) != tolower(s[right]))
+            return false;
+
+        return check(s, left + 1, right - 1);
+    }
+
+    bool isPalindrome(string s) {
+        return check(s, 0, s.length() - 1);
+    }
+};
